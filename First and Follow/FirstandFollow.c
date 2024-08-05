8c269a19d6e4ca60130013e9acccea1259ae4884
avin madhu <84233047+avin-madhu@users.noreply.github.com>
2024-08-05 21:19:37 +0530

    first and follow edited

--- First and Follow/FirstandFollow.c
+++ First and Follow/FirstandFollow.c
@@ -1,117 +1,113 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-
-int number_of_productions, choice, r=0;
-char productions[30][30], ch, result[30];
-void first(char c);
-void follow(char c);
-
+int n,r,p;
+char productions[30][30],nl,input,result[30],already_printed[10];
+int already_printed_result(char c)
+{
+   for(int i=0;i<p;i++)
+   {
+      if(already_printed[i] == c)
+         return 1;
+   }
+   return 0;
+}
 void add_to_result(char c)
 {
-   for(int i=0;i<r;i++)
-   {
-    if(c == result[i])
-    {
-      return;
-    }
-   }
-   result[r++] = c;
+  for(int i=0;i<r;i++)
+  {
+    if(result[i] == c)
+        return;
+  }
+  result[r++] = c;
 }
-
 void first(char c)
 {
-  if(islower(c) || c == '#')
+  if(!isupper(c))
   {
-    add_to_result(c);
-    return;
+     add_to_result(c);
+     return;
   }
-
-  for(int i=0;i<number_of_productions;i++)
+  for(int i=0;i<n;i++)
   {
     if(productions[i][0] == c)
     {
-      if(productions[i][2] == '#')
-      {
-        add_to_result('#');
-      }
-      else if (productions[i][2] == c)
-      {
-        return;
-      }
-      else if(islower(productions[i][2]))
-      {
-        add_to_result(productions[i][2]);
-      }
+      if(productions[i][2] == c)
+         continue;
+      else if(!isupper(productions[i][2]))
+         add_to_result(productions[i][2]);
       else
-      {
          first(productions[i][2]);
-      }
     }
-  }
+  } 
 }
-
 void follow(char c)
 {
-   if(productions[0][0] == c)
-   {
-      add_to_result('$');
-   }
-   for(int i=0;i<number_of_productions;i++)
-   {
+  if(productions[0][0] == c)
+     add_to_result('$');
+  for(int i=0;i<n;i++)
+  {
     for(int j=2;j<strlen(productions[i]);j++)
     {
       if(productions[i][j] == c)
       {
-        if(productions[i][j+1] != '\0')
-        {
-          first(productions[i][j+1]);
-        }
-        if(productions[i][j+1] == '\0' && productions[i][0] != c)
-        {
-          follow(productions[i][0]);
-        }
+        if(productions[i][j+1] == '\0')
+           follow(productions[i][0]);
+        else if (productions[i][j+1] == c)
+           continue;
+        else if(!isupper(productions[i][j+1]) && productions[i][j] != '#')
+           add_to_result(productions[i][j+1]);
+        else
+           first(productions[i][j+1]);
       }
     }
-   }
+  }
 }
-
 int main()
 {
-  int input;
-  printf("Enter the number of productions: ");
-  scanf("%d", &number_of_productions);
-
-  printf("Enter the productions: \n");
-  for(int i=0;i<number_of_productions;i++)
-  {
-     scanf("%s%c", &productions[i], &ch);
-  }
-  do
-  {
-    r = 0;
-    printf("Enter the character: ");
-    scanf("%c", &input);
-    first(input);
-    printf("First(%c): {",input);
-    for(int i=0;i<r;i++)
-    {
-      printf("%c,",result[i]);
-    }
-    printf("}\n");
-
-    strcpy(result, " ");
-
-    r = 0;
-    follow(input);
-    printf("Follow(%c): {",input);
-    for(int i=0;i<r;i++)
-    {
-      printf("%c,",result[i]);
-    }
-    printf("}\n");
-    printf("Wanna continue? (1/0): ");
-    scanf("%d%c", &choice,&ch);
-  } while (choice);
-  return 0;
+   printf("Enter the number of productions: ");
+   scanf("%d",&n);
+   printf("Enter the productions: \n\n");
+   for(int i=0;i<n;i++)
+   {
+    scanf("%s%c", productions[i],&nl);
+   }
+   printf("\n");
+   p=0;
+   for(int i=0;i<n;i++)
+   {
+      input = productions[i][0];
+      if(!already_printed_result(input))
+      {
+         r=0;
+         first(input);
+         printf("First of %c: ",input);
+         for(int i=0;i<n;i++)
+         {
+            printf("%c ",result[i]);
+         }
+         already_printed[p++] = input;
+         printf("\n");
+      }
+   }
+   p=0;
+   printf("==================\n");
+   strcpy(result,"");
+   for(int i=0;i<n;i++)
+   {
+      input = productions[i][0];
+      if(!already_printed_result(input))
+      {
+         r=0;
+         follow(input);
+         printf("Follow of %c: ",input);
+         for(int i=0;i<n;i++)
+         {
+            printf("%c ",result[i]);
+         }
+         already_printed[p++] = input;
+         printf("\n");
+      }
+   }
+   return 0;
 }
