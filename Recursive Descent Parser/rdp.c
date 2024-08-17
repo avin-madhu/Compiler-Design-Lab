d06ee252e2ced0fabccf5d953f92ae599ea26cc7
avin madhu <84233047+avin-madhu@users.noreply.github.com>
2024-08-17 15:16:31 +0530

    recursive decent parser

--- Recursive Descent Parser/rdp.c
+++ Recursive Descent Parser/rdp.c
@@ -1,123 +1,107 @@
-// C program to Construct of recursive descent parsing for
+// C program to Construct a recursive descent parsing for
 // the following grammar
 // E->TE’
-// E’->+TE/@
+// E’->+TE'/@
 // T->FT’
-// T`->*FT’/@
-// F->(E)/id where @ represents null character
+// T’->*FT’/@
+// F->(E)/id 
 
-#include<stdio.h>
-#include<ctype.h>
-#include<string.h>
+//where @ represents null character
+
+#include <stdio.h>
 char input[100];
-int i, l;
+int i=0;
 
 int E();
-int EP();
 int T();
+int EP();
 int TP();
 int F();
 
-
-void main()
+int main()
 {
-    printf("\nRecursive descent parsing for the following grammar\n");
-    printf("\nE->TE'\nE'->+TE'/@\nT->FT'\nT'->*FT'/@\nF->(E)/ID\n");
-    printf("\nEnter the string to be checked:");
+    printf("Enter the input string: ");
     scanf("%s", input);
-    if (E())
-    {
-        if (input[i + 1] == '\0')
-            printf("\nString is accepted");
-        else
-            printf("\nString is not accepted");
-    }
+
+    if(E() && input[i] == '\0')
+       printf("The string is accepted!");
     else
-        printf("\nString not accepted");
+       printf("The string is rejected!");
+    return 0;
 }
+
+
 int E()
 {
-    if (T())
-    {
-        if (EP())
-            return (1);
-        else
-            return (0);
-    }
-    else
-        return (0);
+   if(T())
+   {
+     return EP();
+   }
+   else
+     return 0;
 }
-int EP()
-{
-    if (input[i] == '+')
-    {
-        i++;
-        if (T())
-        {
-            if (EP())
-                return (1);
-            else
-                return (0);
-        }
-        else
-            return (0);
-    }
-    else
-        return (1);
-}
+
 int T()
 {
-    if (F())
-    {
-        if (TP())
-            return (1);
-        else
-            return (0);
-    }
-    else
-        return (0);
+  if(F())
+  {
+    return TP();
+  }
+  else
+    return 0;
 }
+
 int TP()
 {
-    if (input[i] == '*')
-    {
-        i++;
-        if (F())
-        {
-            if (TP())
-                return (1);
-            else
-                return (0);
-        }
-        else
-            return (0);
-    }
-    else
-        return (1);
+   if(input[i] == '*')
+   {
+     i++;
+     if(F())
+        return TP();
+     else
+        return 0;
+   }
+   else
+     return 1;
 }
+
+int EP()
+{
+   if(input[i] == '+')
+   {
+     i++;
+     if(T())
+        return EP();
+     else
+        return 0;
+   }
+   else
+     return 1;
+}
+
 int F()
 {
-    if (input[i] == '(')
+    if(input[i] == '(')
     {
         i++;
-        if (E())
+        if(E())
         {
-            if (input[i] == ')')
+            if(input[i] == ')')
             {
                 i++;
-                return (1);
+                return 1;
             }
             else
-                return (0);
+              return 0;
         }
-        else
-            return (0);
+        else 
+           return 0;
     }
-    else if (input[i] >= 'a' && input[i] <= 'z' || input[i] >= 'A' && input[i] <= 'Z')
+    else if((input[i] >= 'a' && input[i] <= 'z') || (input[i] >= 'A' && input[i] <= 'Z'))
     {
-        i++;
-        return (1);
+       i++;
+       return 1;
     }
     else
-        return (0);
+       return 0;
 }
