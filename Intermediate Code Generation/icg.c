7add05c267404a77afa31d56e1c0cd077a42bb36
avin madhu <84233047+avin-madhu@users.noreply.github.com>
2024-09-23 13:07:51 +0530

    intermediate code gen corrected

--- Intermediate Code Generation/icg.c
+++ Intermediate Code Generation/icg.c
@@ -1,98 +1,129 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<stdlib.h>
-#include <string.h>
+#include<conio.h>
+#include<string.h>
 
-int i = 1, j = 0, no = 0, tmpch = 90;
-
+int i=1, j=0, no=0, tmpch=90;
 char str[100], left[15], right[15];
 
-void find_operator();
-void process();
-void find_left_operand(int);
-void find_right_operand(int);
+void findopr();
+void explore();
+void fleft(int);
+void fright(int);
 
 struct exp {
     int pos;
     char op;
+    int isUnary;
 } k[15];
 
-void clear(char arr[])
-{
-    for(int i=0;i<sizeof(arr);i++)
-    {
-       arr[i] = '\0';  
+void main() {
+    printf("\t\tINTERMEDIATE CODE GENERATION\n\n");
+    printf("Enter the Expression : ");
+    scanf("%s", str);
+    printf("The intermediate code:\n");
+    findopr();
+    explore();
+}
+
+void findopr() {
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '(' || str[i] == ')') {
+            k[j].pos = i;
+            k[j].isUnary=0;
+            k[j++].op = str[i];
+        }
+    }
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '-' && (i == 0 || (str[i-1] == '(' || str[i-1] == '+' || str[i-1] == '-' || str[i-1] == '*' || str[i-1] == '/'))) {
+            k[j].pos = i;
+            k[j].isUnary=1;
+            k[j++].op = '-';
+        }
+    }
+    
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '/' || str[i] == '*') {
+            k[j].pos = i;
+            k[j].isUnary=0;
+            k[j++].op = '/';
+        }
+    }
+    
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '+') {
+            k[j].pos = i;
+            k[j].isUnary=0;
+            k[j++].op = '+';
+        }
+    }
+    
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '-') {
+            if (i == 0 || (str[i-1] == '(' || str[i-1] == '+' || str[i-1] == '-' || str[i-1] == '*' || str[i-1] == '/')) {
+                // Unary minus detected (after '(', '+', '-', '*', or '/')
+                continue;
+            }
+            k[j].pos = i;
+            k[j].isUnary=0;
+            k[j++].op = '-';
+        }
     }
 }
 
-int main() {
-    printf("\t\tINTERMEDIATE CODE GENERATION\n\n");
-    printf("Enter the Expression :");
-    scanf("%s", str);
-    printf("The intermediate code:\n");
-    find_operator();
-    process();
-    return 0;
-}
-
-void find_operator() {
-    
-    for (i = 0; str[i] != '\0'; i++)
-        if (str[i] == '*' || str[i] == '/')
-        {
-            k[j].pos = i;
-            k[j++].op = str[i];
-            no++;
-        }
-
-    for (i = 0; str[i] != '\0'; i++)
-        if (str[i] == '+' || str[i] == '-')
-        {
-            k[j].pos = i;
-            k[j++].op = str[i];
-            no++;
-        }
-}
-
-void process() {
+void explore() {
     i = 0;
-    while (i < j) {
-        find_left_operand(k[i].pos);
-        find_right_operand(k[i].pos);
-        str[k[i].pos] = tmpch--; 
-        printf("\t%c := %s%c%s\t\t", str[k[i].pos], left, k[i].op, right);
+    while (k[i].op != '\0') {
+        if (k[i].op == '(' || k[i].op == ')') {
+            // Skip parentheses
+            i++;
+            continue;
+        }
+        if (k[i].isUnary) {
+            fright(k[i].pos);
+            str[k[i].pos] = tmpch--;
+            printf("\t%c := %c %s\t\t", str[k[i].pos], k[i].op, right);
+        }
+        else{
+            fleft(k[i].pos);
+            fright(k[i].pos);
+            str[k[i].pos] = tmpch--; // Replace the operator with temporary variable
+            
+            // Print intermediate code
+            printf("\t%c := %s %c %s\t\t", str[k[i].pos], left, k[i].op, right);
+        }
         printf("\n");
         i++;
     }
+    
+    fright(-1);
 }
 
-void find_left_operand(int x) {
-    clear(left);
+void fleft(int x) {
     int w = 0, flag = 0;
     x--;
-
-    while (x != -1 && str[x] != '+' && str[x] != '*' && str[x] != '=' &&
-           str[x] != '\0' && str[x] != '-' && str[x] != '/' && str[x] != ':') {
+    
+    while (x != -1 && str[x] != '+' && str[x] != '*' && str[x] != '=' && str[x] != '\0' && str[x] != '-' && str[x] != '/' && str[x] != ':') {
         if (str[x] != '$' && flag == 0) {
             left[w++] = str[x];
             left[w] = '\0';
-            str[x] = '$'; 
+            str[x] = '$'; // Mark the operand as processed
             flag = 1;
         }
         x--;
     }
 }
 
-void find_right_operand(int x) {
-    clear(right);
+void fright(int x) {
     int w = 0, flag = 0;
     x++;
-
-    while (x != -1 && str[x] != '+' && str[x] != '*' && str[x] != '\0' &&
-           str[x] != '=' && str[x] != ':' && str[x] != '-' && str[x] != '/' && j != strlen(str)+1) {
+    
+    while (x != -1 && str[x] != '+' && str[x] != '*' && str[x] != '\0' && str[x] != '=' && str[x] != '-' && str[x] != '/' && str[x] != ':') {
         if (str[x] != '$' && flag == 0) {
             right[w++] = str[x];
             right[w] = '\0';
-            str[x] = '$';  
+            str[x] = '$'; // Mark the operand as processed
             flag = 1;
         }
         x++;
