745b78e1d22ae5e2bb34688a93a43c259c1f49ee
avin madhu <84233047+avin-madhu@users.noreply.github.com>
2024-11-12 12:43:39 +0530

    constant prop i/o

--- Constant Propagation/cp.c
+++ Constant Propagation/cp.c
@@ -1,76 +1,68 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <stdlib.h>
-void input(); // to get the expression from the users (format: + 1 2 3)
-void constant(); // fold expressions to constants
-void output();
-void change(int p, char *res);
+
 int n;
 
-struct expr {
-    char op[2], op1[5], op2[5], res[5];
-    int flag;
-} arr[10];
+void input();
+void check();
+void change(int p, char *res);
+void output();
 
-int main() {
-    input();
-    constant();
-    output();
+struct expression{
+    char op1[10],op[2],op2[10],res[10];
+    int folded;
+}arr[10];
+
+int main(){
+    input(); check(); output();
     return 0;
 }
 
-void input() {
-    printf("Enter the maximum number of expressions: ");
-    scanf("%d",&n);
-    printf("Enter the input: \n");
-    for (int i = 0; i<n ; i++)
-    {
-        scanf("%s", arr[i].op);
-        scanf("%s", arr[i].op1);
-        scanf("%s", arr[i].op2);
-        scanf("%s", arr[i].res);
-        arr[i].flag = 0;
+void input(){
+    printf("Enter the number of expression: ");
+    scanf("%d", &n);
+    printf("Enter the expression: \n");
+    for(int i=0;i<n;i++){
+        scanf("%s %s %s %s", arr[i].op1, arr[i].op, arr[i].op2, arr[i].res);
+        arr[i].folded = 0;
     }
 }
-
-void constant() {
-    int op1,op2,res;
-    char op, res1[5];
-    for(int i=0; i<n; i++) {
-        if ((isdigit(arr[i].op1[0]) && isdigit(arr[i].op2[0])) || strcmp(arr[i].op, "=") == 0) {
-            op1 = atoi(arr[i].op1);
-            op2 = atoi(arr[i].op2);
-            op  = arr[i].op[0];
-
-            switch (op){
-                case '+': res = op1 + op2; break;
-                case '-': res = op1 - op2; break;
-                case '*': res = op1 * op2; break;
-                case '/': res = op1 / op2; break;
-                case '=': res = op1; break;
+void check(){
+    for(int i=0;i<n;i++){
+        if((isdigit(arr[i].op1[0]) && isdigit(arr[i].op2[0])) || strcmp("=", arr[i].op) == 0){
+            int result;
+            int operand1 = atoi(arr[i].op1);
+            int operand2 = atoi(arr[i].op2);
+            char operand = arr[i].op[0];
+            switch(operand){
+                case '+': result = operand1 + operand2; break;
+                case '-': result = operand1 - operand2; break;
+                case '*': result = operand1 * operand2; break;
+                case '/': result = operand1 / operand2; break; 
             }
-            sprintf(res1, "%d", res);
-            arr[i].flag = 1; // ignore this expression like bruh!
-            change(i,res1);
+            arr[i].folded = 1;
+            char res_char[5];
+            sprintf(res_char, "%d", result);
+            change(i,res_char);
         }
     }
 }
 
-void output() {
-    printf("\n Optimzied code is: ");
-    for(int i=0; i<n; i++) {
-        if(arr[i].flag == 0) {
-            printf("\n%s %s %s %s ",arr[i].op1,arr[i].op,arr[i].op2,arr[i].res);
-        }
+void change(int p, char *res){
+    for(int i=p+1;i<n;i++){
+        if(strcmp(arr[i].op1, arr[p].res)==0)
+            strcpy(arr[i].op1, res);
+        if(strcmp(arr[i].op2, arr[p].res)==0)
+            strcpy(arr[i].op2, res);
     }
 }
 
-void change(int p, char *res) {
-    for(int i=p+1; i<n; i++) {
-        if (strcmp(arr[p].res, arr[i].op1)==0)
-            strcpy(arr[i].op1,res);
-        else if(strcmp(arr[p].res, arr[i].op2)==0)
-            strcpy(arr[i].op2,res);
+void output(){
+    printf("\noptimized code: \n");
+    for(int i=0;i<n;i++){
+        if(arr[i].folded == 0)
+            printf("%s %s %s %s\n", arr[i].op1, arr[i].op, arr[i].op2, arr[i].res);
     }
 }
